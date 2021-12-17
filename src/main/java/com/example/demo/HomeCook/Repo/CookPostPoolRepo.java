acc2bcccb0fc39c9365181984e7bc6a7ba1545d2
nghiale <trongnghialedinh@gmail.com>
2021-12-17 20:52:12 +0700

    fix some queries

--- src/main/java/com/example/demo/HomeCook/Repo/CookPostPoolRepo.java
+++ src/main/java/com/example/demo/HomeCook/Repo/CookPostPoolRepo.java
@@ -14,11 +14,13 @@ import java.util.ArrayList;
 public interface CookPostPoolRepo extends JpaRepository<CookPostPool, UserReactCookPostIdClass> {
     @Query(value = "SELECT * FROM cook_post_pool WHERE cook_post_id IN " +
             "(SELECT a.cook_post_id FROM (cook_post_pool AS a INNER JOIN cuisine_posts AS b ON b.id=a.cook_post_id) WHERE " +
-            "a.user_id=:userId AND b.is_deleted=FALSE AND (:searchText IS NULL OR :searchText='' OR LOWER(b.title) LIKE %:searchText%)) " +
+            "b.is_deleted=FALSE AND (:searchText IS NULL OR :searchText='' OR LOWER(b.title) LIKE %:searchText%)) " +
+            "AND a.user_id=:userId " +
             "ORDER BY added_date",
             countQuery = "SELECT COUNT(*) FROM cook_post_pool WHERE cook_post_id IN " +
                     "(SELECT a.cook_post_id FROM (cook_post_pool AS a INNER JOIN cuisine_posts AS b ON b.id=a.cook_post_id) WHERE " +
-                    "a.user_id=:userId AND b.is_deleted=FALSE AND (:searchText IS NULL OR :searchText='' OR LOWER(b.title) LIKE %:searchText%)) " +
+                    "b.is_deleted=FALSE AND (:searchText IS NULL OR :searchText='' OR LOWER(b.title) LIKE %:searchText%)) " +
+                    "AND a.user_id=:userId " +
                     "ORDER BY added_date",
             nativeQuery = true)
     ArrayList<CookPostPool> findAllByUser(@Param("userId") int userId,
